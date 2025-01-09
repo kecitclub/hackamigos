444837a0c3dc40cf06055690678ea6515e49aa05
unknown <raunakrajadh@gmail.com>
2025-01-09 23:25:44 +0545

    updates

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ let port = process.env.PORT || 3000;
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.set('view engine', 'ejs')
+app.use('/assets', express.static('assets'));
 app.use('/', require('./routes/_routes.js').app)
 
 server.listen(port, () => {
