b5e85a4da427b8d1c52257e95c97340c7cb11002
unknown <raunakrajadh@gmail.com>
2025-01-10 14:08:15 +0545

    concept completed

--- functions.js
+++ functions.js
@@ -28,7 +28,81 @@ module.exports = {
         }
     },
 
-    mix: async (chemical1, chemical2) =>{
-        return await module.exports.openAI(`give me one array with ["result", "color of reaction"] on what results you get when you mix ${chemical1} and ${chemical2} say one word for result and color or colorless`)
+    mix: {
+        getChemicalReaction: async (chemical1, chemical2) => {
+            let d = await module.exports.openAI(`give me a straight answer of chemical reaction when you mix ${chemical1} and ${chemical2}`)
+            return d;
+        },
+
+        getColor: async (chemical1, chemical2) => {
+            let d = await module.exports.openAI(`give me a straight answer one word hex color e.g #FFF or #000 of product when you mix ${chemical1} and ${chemical2}`)
+            return d;
+        },
+
+        getProduct: async (chemical1, chemical2) => {
+            let d = await module.exports.openAI(`give me a straight answer one word name of the chemical product when you mix ${chemical1} and ${chemical2}`)
+            return d;
+        }
+    },
+
+    list: [
+        ['Copper sulfate', 'blue'],
+        ['Potassium permanganate', 'purple'],
+        ['Ferric chloride', 'yellow'],
+        ['Chromium trioxide', 'orange'],
+        ['Nickel sulfate', 'green'],
+        ['Potassium dichromate', 'orange'],
+        ['Cobalt chloride', 'pink'],
+        ['Lead iodide', 'yellow'],
+        ['Iron(II) sulfate', 'green'],
+        ['Iron(III) nitrate', 'yellow'],
+        ['Sodium chromate', 'yellow'],
+        ['Sodium dichromate', 'orange'],
+        ['Silver nitrate', 'white'],
+        ['Copper chloride', 'green'],
+        ['Potassium ferricyanide', 'red'],
+        ['Potassium ferrocyanide', 'yellow'],
+        ['Manganese dioxide', 'black'],
+        ['Methyl orange', 'orange'],
+        ['Phenolphthalein', 'white'],
+        ['Ammonium dichromate', 'orange'],
+        ['Sodium thiosulfate', 'white'],
+        ['Bromine', 'red'],
+        ['Chlorine', 'green'],
+        ['Iodine', 'purple'],
+        ['Sulfur', 'yellow'],
+        ['Zinc oxide', 'white'],
+        ['Lead oxide', 'yellow'],
+        ['Antimony trioxide', 'white'],
+        ['Cobalt nitrate', 'red'],
+        ['Prussian blue', 'blue'],
+        ['Cadmium sulfide', 'yellow'],
+        ['Copper(I) oxide', 'red'],
+        ['Mercuric sulfide', 'red'],
+        ['Uranium oxide', 'green'],
+        ['Vanadium pentoxide', 'orange'],
+        ['Zinc sulfide', 'white'],
+        ['Calcium carbonate', 'white'],
+        ['Manganese(II) sulfate', 'pink'],
+        ['Bismuth nitrate', 'white'],
+        ['Titanium dioxide', 'white'],
+        ['Thymol blue', 'blue'],
+        ['Bromothymol blue', 'blue'],
+        ['Indigo carmine', 'blue'],
+        ['Crystal violet', 'purple'],
+        ['Eosin Y', 'pink'],
+        ['Fluorescein', 'green'],
+        ['Malachite green', 'green'],
+        ['Rhodamine B', 'pink'],
+        ['Methylene blue', 'blue']
+    ],
+
+    getColor: (chemicalName) => {
+        for (let item of module.exports.list) {
+            if (item[0].toLowerCase() == chemicalName.toLowerCase()) {
+                return item[1];
+            }
+        }
+        return false;
     }
 }
